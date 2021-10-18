cfe70d386f81b5defa2e94ddb4e4fcbe3a383223
woodystock <91628945+woodystock@users.noreply.github.com>
2021-10-18 15:48:21 +0100

    init

---
+++ src/romanNumerals.js
@@ -0,0 +1,14 @@
+
+// a function to convert a number into a roman numeral
+const numberToRomanNumeral = num => {
+    if(isNaN(num))  throw new Error("num (as number) is required")
+    
+
+}
+
+// a function to convert a roman numeral string into a number
+const romanNumeralToNumber = romanNumeralStr => {
+    if(! romanNumeralStr ) throw new Error("romanNumeralStr (as string) is required")
+
+
+}
