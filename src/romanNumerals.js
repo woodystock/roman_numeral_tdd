a9519d6d364eb7de05100ecb911622786e73f1fe
woodystock <91628945+woodystock@users.noreply.github.com>
2021-10-18 19:32:30 +0100

    all up to 50 working
    This is where I got up to in the video as I forgot to commit live!

--- src/romanNumerals.js
+++ src/romanNumerals.js
@@ -2,8 +2,25 @@
 // a function to convert a number into a roman numeral
 const numberToRomanNumeral = num => {
     if(isNaN(num))  throw new Error("num (as number) is required")
-    
 
+    const romanNumeralsOrder = [[10,"X"],[5,"V"],[1,"I"]];
+    
+    let romanStr = "";
+    let index = 0;
+
+    while (index < romanNumeralsOrder.length)
+    {
+        if(num >= romanNumeralsOrder[index][0])
+        {
+            romanStr += romanNumeralsOrder[index][1];
+            num -= romanNumeralsOrder[index][0];
+        }
+        else{
+            index++;
+        }
+    }
+
+    return romanStr;
 }
 
 // a function to convert a roman numeral string into a number
@@ -11,4 +28,9 @@ const romanNumeralToNumber = romanNumeralStr => {
     if(! romanNumeralStr ) throw new Error("romanNumeralStr (as string) is required")
 
 
+}
+
+module.exports = {
+    numberToRomanNumeral,
+    romanNumeralToNumber
 }
